3aac77c9cdb9a05ba21d9adbb4480d050d9a203b
hiroyamaguch <hiroyuki_yamaguchi@hotmail.com>
2020-05-29 01:13:27 -0300

    Autenticação da aplicação Web

--- frontend/src/pages/SignIn/index.tsx
+++ frontend/src/pages/SignIn/index.tsx
@@ -1,10 +1,10 @@
-import React, { useCallback, useRef, useContext } from 'react';
+import React, { useCallback, useRef } from 'react';
 import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
 import { FormHandles } from '@unform/core';
 import { Form } from '@unform/web';
 import * as Yup from 'yup';
 
-import { AuthContext } from '../../context/AuthContext';
+import { useAuth } from '../../hooks/AuthContext';
 import getValidationErrors from '../../utils/getValidationErrors';
 
 import Button from '../../components/Button';
@@ -21,7 +21,8 @@ interface SignInData {
 const SignIn: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
 
-  const { signin } = useContext(AuthContext);
+  const { signin } = useAuth();
+
   const handleSubmit = useCallback(
     async (data: SignInData) => {
       try {
